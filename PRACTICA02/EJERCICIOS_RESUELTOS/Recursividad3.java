5282a648e3c3ddfff49d57533db4d6ed48b4d9ea
AntonyCondori <acondoricai@unsa.edu.pe>
2025-05-17 22:42:15 -0500

    Subiendo el ejercicio resuelto 3 y el ejercicio resuleto 2 debido a un error que tuve

--- PRACTICA02/EJERCICIOS_RESUELTOS/Recursividad3.java
+++ PRACTICA02/EJERCICIOS_RESUELTOS/Recursividad3.java
@@ -1,12 +1,15 @@
 package PRACTICA02.EJERCICIOS_RESUELTOS;
 
-public class Recursividad2 {
+public class Recursividad3 {
     void imprimir(int x) {
+        if (x > 0) {
         System.out.println(x);
         imprimir(x - 1);
+        }
     }
     public static void main(String[] ar) {
-        Recursividad2 re = new Recursividad2();
+        Recursividad3 re = new Recursividad3();
         re.imprimir(5);
     }
+        
 }
