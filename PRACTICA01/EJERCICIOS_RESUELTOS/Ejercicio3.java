e5825392049a25f5d66727d87ddf30567068b143
AntonyCondori <acondoricai@unsa.edu.pe>
2025-05-17 21:48:40 -0500

    Subiendo el ejercicio resuelto 3

---
+++ PRACTICA01/EJERCICIOS_RESUELTOS/Ejercicio3.java
@@ -0,0 +1,28 @@
+package PRACTICA01.EJERCICIOS_RESUELTOS;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el número de elementos: ");
+        int n = scanner.nextInt();
+        int[] numeros = new int[n];
+
+        System.out.println("Ingrese los números:");
+        for (int i = 0; i < n; i++) {
+            numeros[i] = scanner.nextInt();
+        }
+        boolean estaOrdenada = true; // Invariante: Se supone que la lista está ordenada
+        for (int i = 1; i < n; i++) {
+            if (numeros[i] < numeros[i - 1]) {
+                estaOrdenada = false; // Se rompe la invariante si encontramos un desorden
+                break;
+            }
+        }
+        System.out.println("¿Está ordenada la lista?: " + (estaOrdenada ? "Sí" : "No"));
+        
+        scanner.close();
+    }
+}
