0069992f6904e01aa14381fe331d0b88b81ddc96
AntonyCondori <acondoricai@unsa.edu.pe>
2025-05-17 22:42:59 -0500

    Subiendo el ejercicio resuelto 4

---
+++ PRACTICA02/EJERCICIOS_RESUELTOS/Recursividad4.java
@@ -0,0 +1,14 @@
+package PRACTICA02.EJERCICIOS_RESUELTOS;
+
+public class Recursividad4 {
+    void imprimir(int x) {
+        if (x > 0) {
+        imprimir(x - 1);
+        System.out.println(x);
+        }
+    }
+    public static void main(String[] ar) {
+        Recursividad4 re = new Recursividad4();
+        re.imprimir(5);
+    }
+}
