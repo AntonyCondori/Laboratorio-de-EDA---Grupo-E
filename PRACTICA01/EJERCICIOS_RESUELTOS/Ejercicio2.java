3cab73260d0e612bc13844a60a1bd37d62bd5c19
AntonyCondori <acondoricai@unsa.edu.pe>
2025-05-17 21:27:58 -0500

    Subiendo el ejercicio resuelto 2

---
+++ PRACTICA01/EJERCICIOS_RESUELTOS/Ejercicio2.java
@@ -0,0 +1,22 @@
+package PRACTICA01.EJERCICIOS_RESUELTOS;
+
+import java.util.*;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número N: ");
+        int n = scanner.nextInt();
+        int suma = 0, contador = 1;
+
+        while (contador <= n) {
+            suma += contador;
+            contador++;
+        }
+
+        System.out.println("La suma de los primeros " + n + " números naturales es: " + suma);
+        
+        scanner.close();
+    }
+}
