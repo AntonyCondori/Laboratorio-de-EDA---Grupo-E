14690751a418f4d8e89c4d6f268f17fb670b17e2
AntonyCondori <acondoricai@unsa.edu.pe>
2025-05-31 22:39:31 -0500

    Subiendo ejercicio resuelto 11

---
+++ PRACTICA04/EJERCICIOS_RESUELTOS/GFG6.java
@@ -0,0 +1,12 @@
+package PRACTICA04.EJERCICIOS_RESUELTOS;
+import java.io.*;
+import java.util.LinkedList;
+public class GFG6 {
+    public static void main(String args[]) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("Uno, Dos, Tres ");
+        list.add("Cuatro ");
+        // Mostrar el tamaño de la lista
+        System.out.println("El tamaño de la lista es: " + list.size());
+    }
+}
