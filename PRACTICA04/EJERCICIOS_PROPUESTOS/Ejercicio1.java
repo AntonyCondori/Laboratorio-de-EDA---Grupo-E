6db3dfe72c54d94f0213b3982380ed811ed7c70c
AntonyCondori <acondoricai@unsa.edu.pe>
2025-05-31 23:15:15 -0500

    Corrijiendo el ejercicio propuesto 1

--- PRACTICA04/EJERCICIOS_PROPUESTOS/Ejercicio1.java
+++ PRACTICA04/EJERCICIOS_PROPUESTOS/Ejercicio1.java
@@ -1,50 +1,46 @@
 package PRACTICA04.EJERCICIOS_PROPUESTOS;
 
-public class Ejercicio1 {
-    Nodo inicio, fin;
+public class Ejercicio1<E> {
+    Nodo<E> inicio, fin;
 
-    // Inserta un nuevo nodo al final de la lista
-    public void insertarAlFinal(int valor) {
-        Nodo nuevoNodo = new Nodo(valor);
+    public void insertarAlFinal(E valor) {
+        Nodo<E> nuevoNodo = new Nodo<>(valor);
         if (inicio == null) {
             inicio = fin = nuevoNodo;
         } else {
-            fin.nodoSiguiente = nuevoNodo;
-            nuevoNodo.nodoAnterior = fin;
+            fin.siguiente = nuevoNodo;
+            nuevoNodo.anterior = fin;
             fin = nuevoNodo;
         }
     }
 
-    // Muestra la lista desde el inicio hasta el final
     public void mostrarLista() {
-        Nodo actual = inicio;
+        Nodo<E> actual = inicio;
         System.out.print("Lista doblemente enlazada: ");
         while (actual != null) {
             System.out.print(actual.valor + " ");
-            actual = actual.nodoSiguiente;
+            actual = actual.siguiente;
         }
         System.out.println();
     }
 
     public static void main(String[] args) {
-        Ejercicio1 lista = new Ejercicio1();
-
+        Ejercicio1<Integer> lista = new Ejercicio1<>();
         for (int i = 1; i <= 10; i++) {
             lista.insertarAlFinal(i);
         }
-
         lista.mostrarLista();
     }
 }
 
-class Nodo {
-    int valor;
-    Nodo nodoAnterior;
-    Nodo nodoSiguiente;
+class Nodo<E> {
+    E valor;
+    Nodo<E> anterior;
+    Nodo<E> siguiente;
 
-    public Nodo(int valor) {
+    public Nodo(E valor) {
         this.valor = valor;
-        this.nodoAnterior = null;
-        this.nodoSiguiente = null;
+        this.anterior = null;
+        this.siguiente = null;
     }
 }
---
+++ PRACTICA04/EJERCICIOS_PROPUESTOS/Ejercicio3.java
@@ -0,0 +1,5 @@
+package PRACTICA04.EJERCICIOS_PROPUESTOS;
+
+public class Ejercicio3 {
+    
+}
