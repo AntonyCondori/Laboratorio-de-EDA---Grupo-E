14859d20d5ff2fb9fdf2003cb5d0045a274cd52a
AntonyCondori <acondoricai@unsa.edu.pe>
2025-05-31 22:35:21 -0500

    Subiendo el ejercicio 7

---
+++ PRACTICA04/EJERCICIOS_RESUELTOS/GFG2.java
@@ -0,0 +1,13 @@
+package PRACTICA04.EJERCICIOS_RESUELTOS;
+// Programa java para añadir elementos
+// a la LinkedList
+import java.util.*;
+public class GFG2 {
+    public static void main(String args[]) {
+        LinkedList<String> ll = new LinkedList<>();
+        ll.add("Uno");
+        ll.add("Tres");
+        ll.add(1, "Dos");
+        System.out.println(ll);
+    }
+}
