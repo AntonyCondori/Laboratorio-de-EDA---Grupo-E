8bbef6a41e956c2c1f7ca8dcf1ef96016ccf07f6
AntonyCondori <acondoricai@unsa.edu.pe>
2025-05-31 23:22:49 -0500

    Resolviendo el ejercicio propuesto 3

--- PRACTICA04/EJERCICIOS_PROPUESTOS/Ejercicio3.java
+++ PRACTICA04/EJERCICIOS_PROPUESTOS/Ejercicio3.java
@@ -1,5 +1,18 @@
 package PRACTICA04.EJERCICIOS_PROPUESTOS;
+import java.util.LinkedList;
 
 public class Ejercicio3 {
-    
+    public static void main(String[] args) {
+        LinkedList<Integer> lista = new LinkedList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            lista.add(i);
+        }
+
+        System.out.print("Lista doblemente enlazada con java.util: ");
+        for (Integer numero : lista) {
+            System.out.print(numero + " ");
+        }
+        System.out.println();
+    }
 }
